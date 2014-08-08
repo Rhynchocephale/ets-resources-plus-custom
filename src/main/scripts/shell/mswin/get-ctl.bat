221df33e67246c1d8fcbc2e4c3d55af403762adb
R. Martell <rjmartell@computer.org>
2014-08-08 12:22:21 -0700

    Build CTL script assemblies from Git source.

---
+++ src/main/scripts/shell/mswin/get-ctl.bat
@@ -0,0 +1,40 @@
+:: Reads a CSV file (first argument) where each record contains two fields:
+:: Git repository URL, tag name
+:: Example:
+:: https://github.com/opengeospatial/ets-kml22.git,2.2-r10
+::
+:: Note: Maven and Git must be installed and available on the system path
+
+@echo off
+set home=%~dp0
+if exist "%home%setenv.bat" call "%home%setenv.bat"
+if "%JAVA_HOME%"=="" echo JAVA_HOME must be set. & goto :eof
+
+if [%1]==[] echo Location of CSV file not specified. & goto :eof
+set csvfile=%~f1
+
+set repobase=%USERPROFILE%\ets-src
+cd /d %repobase%
+for /F "usebackq tokens=1,2 delims=," %%a in ("%csvfile%") do (
+  if exist %%~na (
+    cd /d %%~na
+    git pull origin
+  )
+  if not exist %%~na (
+    git clone %%a
+    cd /d %%~na
+  )
+  call :buildtag "%%b"
+  cd /d ..
+)
+
+cd /d %TE_BASE%\scripts\
+for %%f in (*.zip) do ("%JAVA_HOME%"\bin\jar xf %%f & del %%f)
+cd /d %home%
+goto :eof
+
+:buildtag
+call git checkout %1
+call mvn -DskipTests clean install
+copy /y target\*-ctl-scripts.zip %TE_BASE%\scripts\
+exit /b
---
+++ src/main/scripts/shell/mswin/setenv.bat
@@ -0,0 +1,5 @@
+rem set TE_BASE=%USERPROFILE%\teamengine
+rem mkdir "%TE_BASE%" "%TE_BASE%\work" "%TE_BASE%\users" "%TE_BASE%\scripts" "%TE_BASE%\resources" 2>NUL
+
+rem set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_67
+rem set JAVA_OPTS=-Xmx256m
