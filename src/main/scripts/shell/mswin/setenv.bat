07e6900659ef41deb57c8362f8af5553c2788062
R. Martell <rjmartell@computer.org>
2015-11-05 12:09:17 -0800

    Incorporate latest OGC test suite releases

--- src/main/scripts/shell/mswin/setenv.bat
+++ src/main/scripts/shell/mswin/setenv.bat
@@ -5,5 +5,5 @@ rem mkdir "%ETS_SRC%" 2>NUL
 rem set TE_BASE=%USERPROFILE%\teamengine
 rem mkdir "%TE_BASE%" "%TE_BASE%\work" "%TE_BASE%\users" "%TE_BASE%\scripts" "%TE_BASE%\resources" 2>NUL
 
-rem set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_67
+rem set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_79
 rem set JAVA_OPTS=-Xmx256m
--- src/main/scripts/shell/unix/setenv.sh
+++ src/main/scripts/shell/unix/setenv.sh
@@ -5,5 +5,5 @@
 #export TE_BASE=$HOME/teamengine
 #mkdir -p $TE_BASE/{work,users,scripts,resources} 2>/dev/null
 
-#JAVA_HOME=/opt/oracle.com/jdk1.7.0_67
+#JAVA_HOME=/opt/oracle.com/jdk1.7.0_79
 #JAVA_OPTS=-Xmx256m
